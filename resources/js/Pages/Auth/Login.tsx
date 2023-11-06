d6525dbb00d77a448312860d5a1df0d4c9f55fc3
CRISTIAN VALENTINI <cristian.valentini@universo.univates.br>
2023-11-06 08:48:56 -0300

    loginSocial_icons

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,16 +1,16 @@
-import {useEffect, FormEventHandler} from 'react';
+import { useEffect, FormEventHandler } from 'react';
 import Checkbox from '@/Components/Checkbox';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import {Head, Link, useForm} from '@inertiajs/react';
-import {useState} from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
+import { useState } from 'react';
 
 
-export default function Login({status, canResetPassword}: { status?: string, canResetPassword: boolean }) {
-    const {data, setData, post, processing, errors, reset} = useForm({
+export default function Login({ status, canResetPassword }: { status?: string, canResetPassword: boolean }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
         email: '',
         password: '',
         remember: false,
@@ -36,18 +36,25 @@ export default function Login({status, canResetPassword}: { status?: string, can
 
     return (
         <GuestLayout user={undefined}>
-            <Head title="Entrar"/>
+            <Head title="Entrar" />
 
-            <div className="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style={{"marginTop": "25vh"}}>
+            <div className="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style={{ "marginTop": "20vh" }}>
                 <div className="flex items-center justify-center">
-                    <span className="ml-2 text-xl text-black">Faça seu login</span>
-
+                    <span className="ml-2 text-xl text-black">Faça seu login na ReKrie</span>
+                </div>
+                <div className="flex items-center justify-center">
+                    <Link
+                        href={route('login')}
+                        className="underline text-sm text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Cadastrar-se?
+                    </Link>
                 </div>
                 {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
                 <form onSubmit={submit}>
                     <div>
-                        <InputLabel htmlFor="email" value="E-mail"/>
+                        <InputLabel htmlFor="email" value="E-mail" className="ml-2"/>
 
                         <TextInput
                             id="email"
@@ -59,11 +66,11 @@ export default function Login({status, canResetPassword}: { status?: string, can
                             isFocused={true}
                             onChange={(e) => setData('email', e.target.value)}
                         />
-                        <InputError message={errors.email} className="mt-2"/>
+                        <InputError message={errors.email} className="mt-2" />
                     </div>
 
                     <div className="mt-3">
-                        <InputLabel htmlFor="password" value="Senha"/>
+                        <InputLabel htmlFor="password" value="Senha" className="ml-2"/>
 
                         <div className="relative">
                             <TextInput
@@ -81,8 +88,8 @@ export default function Login({status, canResetPassword}: { status?: string, can
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 30 20" fill="none">
                                         <g id="style=linear">
                                             <g id="eye-open">
-                                                <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                <path id="vector_2" d="M12 19.27C15.53 19.27 18.82 17.4413 21.11 14.2764C22.01 13.0368 22.01 10.9532 21.11 9.71356C18.82 6.54861 15.53 4.71997 12 4.71997C8.46997 4.71997 5.17997 6.54861 2.88997 9.71356C1.98997 10.9532 1.98997 13.0368 2.88997 14.2764C5.17997 17.4413 8.46997 19.27 12 19.27Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                                <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path id="vector_2" d="M12 19.27C15.53 19.27 18.82 17.4413 21.11 14.2764C22.01 13.0368 22.01 10.9532 21.11 9.71356C18.82 6.54861 15.53 4.71997 12 4.71997C8.46997 4.71997 5.17997 6.54861 2.88997 9.71356C1.98997 10.9532 1.98997 13.0368 2.88997 14.2764C5.17997 17.4413 8.46997 19.27 12 19.27Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             </g>
                                         </g>
                                     </svg>
@@ -90,9 +97,9 @@ export default function Login({status, canResetPassword}: { status?: string, can
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 30 20" fill="none">
                                         <g id="style=linear">
                                             <g id="eye-close">
-                                                <path id="vector" d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round"/>
+                                                <path id="vector" d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" />
                                             </g>
                                         </g>
                                     </svg>
@@ -100,7 +107,7 @@ export default function Login({status, canResetPassword}: { status?: string, can
                             </button>
                         </div>
 
-                        <InputError message={errors.password} className="mt-2"/>
+                        <InputError message={errors.password} className="mt-2" />
                     </div>
 
                     <div className="block mt-4">
@@ -136,6 +143,24 @@ export default function Login({status, canResetPassword}: { status?: string, can
                         {/*        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Criar nova conta?*/}
                         {/*    </Link>*/}
                         {/*</div>*/}
+                    </div>
+                    <div className="flex items-center mt-2">
+                        <div className="border-t border-gray-300 flex-grow"></div>
+                        <div className="mx-2 text-gray-500">ou</div>
+                        <div className="border-t border-gray-300 flex-grow"></div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center mt-2">
+                        <div className="flex justify-center space-x-4" title="Login com Github">
+                            <button className="bg-white rounded-full" title="Login com Facebook">
+                                <img width="50" height="50" src="https://img.icons8.com/fluency/48/facebook-circled.png" alt="facebook-circled" />
+                            </button>
+                            <button className="bg-white rounded-full">
+                                <img width="50" height="50" src="https://img.icons8.com/glyph-neue/64/github.png" alt="github" />
+                            </button>
+                            <button className="bg-white rounded-full" title="Login com Google">
+                                <img width="48" height="48" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo" />
+                            </button>
+                        </div>
                     </div>
                 </form>
             </div>
--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,14 +1,14 @@
-import {useEffect, FormEventHandler} from 'react';
+import { useEffect, FormEventHandler } from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import {Head, Link, useForm} from '@inertiajs/react';
-import {useState} from 'react';
+import { Head, Link, useForm } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function Register() {
-    const {data, setData, post, processing, errors, reset} = useForm({
+    const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
         cpf: '',
         telefone: '',
@@ -58,8 +58,8 @@ export default function Register() {
 
     return (
         <GuestLayout user={undefined}>
-            <Head title="Registrar-se"/>
-            <div className="w-full sm:max-w-md mt-2 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <Head title="Registrar-se" />
+            <div className="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style={{ "marginTop": "20vh" }}>
                 <div className="flex items-center justify-center">
                     <span className="ml-2 text-xl text-black">Crie uma nova conta na ReKrie</span>
                 </div>
@@ -68,7 +68,7 @@ export default function Register() {
                 </div>
                 <form onSubmit={submit} className="mx-auto">
                     <div>
-                        <InputLabel htmlFor="name" value="Nome*" className={marginLabel}/>
+                        <InputLabel htmlFor="name" value="Nome*" className={marginLabel} />
 
                         <TextInput
                             id="name"
@@ -81,10 +81,10 @@ export default function Register() {
                             required
                         />
 
-                        <InputError message={errors.name} className="mt-2"/>
+                        <InputError message={errors.name} className="mt-2" />
                     </div>
                     <div className="mt-2">
-                        <InputLabel htmlFor="email" value="Email*" className={marginLabel}/>
+                        <InputLabel htmlFor="email" value="E-mail*" className={marginLabel} />
 
                         <TextInput
                             id="email"
@@ -97,11 +97,11 @@ export default function Register() {
                             required
                         />
 
-                        <InputError message={errors.email} className="mt-2"/>
+                        <InputError message={errors.email} className="mt-2" />
                     </div>
-                    <div className="mt-2" style={{display: 'flex'}}>
-                        <div style={{flex: 1, marginRight: '10px'}}>
-                            <InputLabel htmlFor="cpf" value="CPF*" className={marginLabel}/>
+                    <div className="mt-2" style={{ display: 'flex' }}>
+                        <div style={{ flex: 1, marginRight: '10px' }}>
+                            <InputLabel htmlFor="cpf" value="CPF*" className={marginLabel} />
 
                             <TextInput
                                 id="cpf"
@@ -115,11 +115,11 @@ export default function Register() {
                                 required
                             />
 
-                            <InputError message={errors.cpf} className="mt-2"/>
+                            <InputError message={errors.cpf} className="mt-2" />
                         </div>
 
-                        <div style={{flex: 1}}>
-                            <InputLabel htmlFor="telefone" value="Telefone" className={marginLabel}/>
+                        <div style={{ flex: 1 }}>
+                            <InputLabel htmlFor="telefone" value="Telefone" className={marginLabel} />
 
                             <TextInput
                                 id="telefone"
@@ -132,12 +132,12 @@ export default function Register() {
                                 onChange={(e) => formatTelefone(e.target.value)}
                             />
 
-                            <InputError message={errors.telefone} className="mt-2"/>
+                            <InputError message={errors.telefone} className="mt-2" />
                         </div>
                     </div>
-                    <div className="mt-2" style={{display: 'flex'}}>
-                        <div style={{flex: 1, marginRight: '10px'}}>
-                            <InputLabel htmlFor="password" value="Senha*" className={marginLabel}/>
+                    <div className="mt-2" style={{ display: 'flex' }}>
+                        <div style={{ flex: 1, marginRight: '10px' }}>
+                            <InputLabel htmlFor="password" value="Senha*" className={marginLabel} />
                             <div className="relative">
                                 <TextInput
                                     id="password"
@@ -154,9 +154,9 @@ export default function Register() {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 30 20" fill="none">
                                             <g id="style=linear">
                                                 <g id="eye-open">
-                                                    <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                                    <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                                     <path id="vector_2" d="M12 19.27C15.53 19.27 18.82 17.4413 21.11 14.2764C22.01 13.0368 22.01 10.9532 21.11 9.71356C18.82 6.54861 15.53 4.71997 12 4.71997C8.46997 4.71997 5.17997 6.54861 2.88997 9.71356C1.98997 10.9532 1.98997 13.0368 2.88997 14.2764C5.17997 17.4413 8.46997 19.27 12 19.27Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round"
-                                                          strokeLinejoin="round"/>
+                                                        strokeLinejoin="round" />
                                                 </g>
                                             </g>
                                         </svg>
@@ -165,20 +165,20 @@ export default function Register() {
                                             <g id="style=linear">
                                                 <g id="eye-close">
                                                     <path id="vector"
-                                                          d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547"
-                                                          stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                    <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                    <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round"/>
+                                                        d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547"
+                                                        stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                    <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                    <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" />
                                                 </g>
                                             </g>
                                         </svg>}
                                 </button>
                             </div>
-                            <InputError message={errors.password} className="mt-2"/>
+                            <InputError message={errors.password} className="mt-2" />
                         </div>
 
-                        <div style={{flex: 1}}>
-                            <InputLabel htmlFor="password_confirmation" value="Confirmar senha*" className={marginLabel}/>
+                        <div style={{ flex: 1 }}>
+                            <InputLabel htmlFor="password_confirmation" value="Confirmar senha*" className={marginLabel} />
                             <div className="relative">
                                 <TextInput
                                     id="password_confirmation"
@@ -198,39 +198,44 @@ export default function Register() {
                                     {isPasswordVisible ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 30 20" fill="none">
                                         <g id="style=linear">
                                             <g id="eye-open">
-                                                <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                                <path id="vector" d="M15 12C15 13.6592 13.6592 15 12 15C10.3408 15 9 13.6592 9 12C9 10.3408 10.3408 9 12 9C13.6592 9 15 10.3408 15 12Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                                 <path id="vector_2" d="M12 19.27C15.53 19.27 18.82 17.4413 21.11 14.2764C22.01 13.0368 22.01 10.9532 21.11 9.71356C18.82 6.54861 15.53 4.71997 12 4.71997C8.46997 4.71997 5.17997 6.54861 2.88997 9.71356C1.98997 10.9532 1.98997 13.0368 2.88997 14.2764C5.17997 17.4413 8.46997 19.27 12 19.27Z" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round"
-                                                      strokeLinejoin="round"/>
+                                                    strokeLinejoin="round" />
                                             </g>
                                         </g>
                                     </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 30 20" fill="none">
                                         <g id="style=linear">
                                             <g id="eye-close">
                                                 <path id="vector"
-                                                      d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547"
-                                                      stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                                <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round"/>
+                                                    d="M15.6487 5.39489C14.4859 4.95254 13.2582 4.72021 12 4.72021C8.46997 4.72021 5.17997 6.54885 2.88997 9.71381C1.98997 10.9534 1.98997 13.037 2.88997 14.2766C3.34474 14.9051 3.83895 15.481 4.36664 16.0002M19.3248 7.69653C19.9692 8.28964 20.5676 8.96425 21.11 9.71381C22.01 10.9534 22.01 13.037 21.11 14.2766C18.82 17.4416 15.53 19.2702 12 19.2702C10.6143 19.2702 9.26561 18.9884 7.99988 18.4547"
+                                                    stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path id="vector_2" d="M15 12C15 13.6592 13.6592 15 12 15M14.0996 9.85541C13.5589 9.32599 12.8181 9 12 9C10.3408 9 9 10.3408 9 12C9 12.7293 9.25906 13.3971 9.69035 13.9166" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path id="vector_3" d="M2 21.0002L22 2.7002" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" />
                                             </g>
                                         </g>
                                     </svg>}
                                 </button>
                             </div>
-                            <InputError message={errors.password_confirmation} className="mt-2"/>
+                            <InputError message={errors.password_confirmation} className="mt-2" />
                         </div>
                     </div>
                     <div className="flex flex-col items-center justify-center mt-4">
                         <PrimaryButton className="w-full h-11 mb-2 flex items-center justify-center" disabled={processing}>
                             Registrar
                         </PrimaryButton>
-                        <div className="flex items-center justify-end mt-4">
-                            <Link
-                                href={route('login')}
-                                className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                                Já é registrado?
-                            </Link>
-                        </div>
+                    </div>
+                    <div className="flex items-center mt-2">
+                        <div className="border-t border-gray-300 flex-grow"></div>
+                        <div className="mx-2 text-gray-500">ou</div>
+                        <div className="border-t border-gray-300 flex-grow"></div>
+                    </div>
+                    <div className="flex items-center justify-center mt-2">
+                        <Link
+                            href={route('login')}
+                            className="underline text-sm text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Já tem cadastro?
+                        </Link>
                     </div>
                 </form>
             </div>
