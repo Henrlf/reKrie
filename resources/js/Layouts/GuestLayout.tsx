099bc84c33c50947b0c2a1e290deb0f1fb764cff
CRISTIAN VALENTINI <cristian.valentini@universo.univates.br>
2023-11-16 16:47:14 -0300

    pageFooter_imagePanel

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,15 +1,16 @@
 import "../../css/ReactToastify.css";
 import "../../css/Custom.css";
-import React, {FormEventHandler, PropsWithChildren} from 'react';
-import {Button, ButtonGroup, Nav, Navbar, Container, NavDropdown, Form} from "react-bootstrap";
-import {toast, ToastContainer} from "react-toastify";
+import React, { FormEventHandler, PropsWithChildren } from 'react';
+import { Button, ButtonGroup, Nav, Navbar, Container, NavDropdown, Form, Row, Col } from "react-bootstrap";
+import { toast, ToastContainer } from "react-toastify";
 import ApplicationIcon from '@/Components/Logos/ApplicationIcon';
-import {faSearch, faCartShopping} from '@fortawesome/free-solid-svg-icons';
-import {User} from "@/types";
-import {useForm} from "@inertiajs/react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { User } from "@/types";
+import { useForm } from "@inertiajs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Guest({user, children}: PropsWithChildren<{ user: any }>) {
+export default function Guest({ user, children }: PropsWithChildren<{ user: any }>) {
     let layout = unloggedLayout();
 
     if (user) {
@@ -24,8 +25,8 @@ export default function Guest({user, children}: PropsWithChildren<{ user: any }>
         <div>
             <header>
                 <Navbar bg="gray-800" data-bs-theme="dark" className="lh-lg">
-                    <Container className="flex-nowrap" style={{"minWidth": "75%"}}>
-                        <Navbar.Brand href="/" className="d-flex"><ApplicationIcon/>ReKrie</Navbar.Brand>
+                    <Container className="flex-nowrap" style={{ "minWidth": "75%" }}>
+                        <Navbar.Brand href="/" className="d-flex"><ApplicationIcon />ReKrie</Navbar.Brand>
                         {layout}
                     </Container>
                 </Navbar>
@@ -47,12 +48,56 @@ export default function Guest({user, children}: PropsWithChildren<{ user: any }>
                 pauseOnHover={false}
                 theme="colored"
             />
+            <footer>
+                <Container className="bg-dark text-light" style={{ "minWidth": "100%" }}>
+                    <Row className="pt-4 justify-content-center">
+                        <Col md={3} className="d-flex justify-content-end">
+                            <div>
+                                <h5>Links Úteis</h5>
+                                <ul className="list-unstyled">
+                                    <li><a href="/sobre">Sobre Nós</a></li>
+                                    <li><a href="mailto:cristian.valentini@universo.univates.br">Contato</a></li>
+                                    <li><a href="/termos">Termos de Serviço</a></li>
+                                    <li><a href="/privacidade">Política de Privacidade</a></li>
+                                </ul>
+                            </div>
+                        </Col>
+                        <Col md={3} className="ml-3 d-flex justify-content-center">
+                            <div>
+                                <h5>Categorias</h5>
+                                <ul className="list-unstyled">
+                                    <li><a href="/categorias/prontaEntrega">À pronta entrega</a></li>
+                                    <li><a href="/categorias/encomendas">Encomendas</a></li>
+                                    {/* Adicione mais categorias conforme necessário */}
+                                </ul>
+                            </div>
+                        </Col>
+                        <Col md={3} className="ml-3 d-flex justify-content-start">
+                            <div>
+                                <h5>Redes Sociais</h5>
+                                <ul className="list-unstyled">
+                                    <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                                    <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                                    <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                                </ul>
+                            </div>
+                        </Col>
+                    </Row>
+                    <hr className="bg-light" />
+                    <Row className="pt-2">
+                        <Col className="text-center">
+                            <p>&copy; 2023 ReKrie. Todos os direitos reservados.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+
         </div>
     );
 }
 
 function unloggedLayout() {
-    const {data, setData, post} = useForm({
+    const { data, setData, post } = useForm({
         searchDescription: '',
     });
 
@@ -78,7 +123,7 @@ function unloggedLayout() {
                 />
 
                 <Button variant="outline-info d-flex align-items-center" onClick={submitSearch}>
-                    <FontAwesomeIcon icon={faSearch}/>
+                    <FontAwesomeIcon icon={faSearch} />
                 </Button>
             </Form>
 
@@ -91,7 +136,7 @@ function unloggedLayout() {
 }
 
 function loggedUserLayout(user: User) {
-    const {data, setData, post} = useForm({
+    const { data, setData, post } = useForm({
         searchDescription: '',
     });
 
@@ -117,20 +162,20 @@ function loggedUserLayout(user: User) {
                 />
 
                 <Button variant="outline-info d-flex align-items-center" title="Pesquisar" onClick={submitSearch}>
-                    <FontAwesomeIcon icon={faSearch}/>
+                    <FontAwesomeIcon icon={faSearch} />
                 </Button>
             </Form>
 
             <Nav className="d-flex justify-content-end align-items-center">
                 <Button href={"/usuario/carrinhocompra"} variant="outline-light" className="nav-btn-size" title="Carrinho de compras">
-                    <FontAwesomeIcon icon={faCartShopping}/>
+                    <FontAwesomeIcon icon={faCartShopping} />
                 </Button>
                 <NavDropdown title={user.name} menuVariant="dark">
                     <NavDropdown.Item href="/usuario/perfil">Perfil</NavDropdown.Item>
                     <NavDropdown.Item href="/usuario/endereco">Endereços</NavDropdown.Item>
                     <NavDropdown.Item href="/usuario/orcamento">Orçamentos</NavDropdown.Item>
                     <NavDropdown.Item href="/usuario/pedido">Meus pedidos</NavDropdown.Item>
-                    <NavDropdown.Divider/>
+                    <NavDropdown.Divider />
                     <NavDropdown.Item href="/auth/logout" eventKey={0}>Sair</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
@@ -139,7 +184,7 @@ function loggedUserLayout(user: User) {
 }
 
 function loggedAdminLayout(user: User) {
-    const {post} = useForm({});
+    const { post } = useForm({});
 
     const enventSelected = (eventKey: any) => {
         if (eventKey == 0) {
@@ -163,7 +208,7 @@ function loggedAdminLayout(user: User) {
                     <NavDropdown.Item href="/">Action</NavDropdown.Item>
                     <NavDropdown.Item href="/">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="/">Something</NavDropdown.Item>
-                    <NavDropdown.Divider/>
+                    <NavDropdown.Divider />
                     <NavDropdown.Item href="/auth/logout" eventKey={0}>Sair</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,16 +1,20 @@
-import {PageProps} from '@/types';
-import React from "react";
+import { PageProps } from '@/types';
+import React, { useEffect, useState } from "react";
 import GuestLayout from '@/Layouts/GuestLayout';
-import {Head} from "@inertiajs/react";
-import {Button, ButtonGroup, Card, Container, Dropdown} from "react-bootstrap";
-import {toast} from "react-toastify";
+import { Head } from "@inertiajs/react";
+import { Button, ButtonGroup, Card, Container, Dropdown } from "react-bootstrap";
+import { toast } from "react-toastify";
 import Separator from "@/Components/Separator";
 import axios from "axios";
+import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
 
-export default function Dashboard({auth, produtos}: PageProps<{ produtos: any }>) {
+
+
+export default function Dashboard({ auth, produtos }: PageProps<{ produtos: any }>) {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const addProdutoCarrinho = (idProduto: any) => {
-        axios.post(route('carrinhocompra.create', {idProduto: idProduto}))
+        axios.post(route('carrinhocompra.create', { idProduto: idProduto }))
             .then(response => {
                 toast.success('Produto adicionado ao carrinho de compras.');
             })
@@ -21,37 +25,84 @@ export default function Dashboard({auth, produtos}: PageProps<{ produtos: any }>
 
     console.log(produtos);
 
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex((prevIndex: number) => (prevIndex + 1) % produtos.length);
+        }, 15000);
+
+        return () => clearInterval(intervalId);
+    }, [produtos.length]);
+
+    const handleImageClick = () => {
+        setCurrentImageIndex((prevIndex: number) => (prevIndex + 1) % produtos.length);
+    };
+
     return (
         <GuestLayout user={auth.user}>
-            <Head title="Dashboard"/>
+            <Head title="Dashboard" />
 
-            <div className="w-70 bg-gray-300">
+            <div className="w-90 bg-gray-300">
                 <Container className="text-center">
-                    <div className="row py-lg-5">
-                        <div className="col-lg-6 col-md-8 mx-auto">
-                            <h1 className="fw-light">Album example</h1>
-                            <p className="lead text-body-secondary">Necessitamos de uma frase de impacto</p>
+                    <div className="col-lg-6 col-md-8 pt-4   mx-auto">
+                        {/* Painel de Imagens */}
+                        <div className="image-panel" style={{ position: 'relative', height: '400px', overflow: 'hidden', borderRadius: '25px' }}>
+                            {produtos.map((produto: any, index: number) => (
+                                <div key={produto.id} style={{ position: 'absolute', width: '100%', height: '100%', display: index === currentImageIndex ? 'block' : 'none' }}>
+                                    <img
+                                        src={produto.imagem}
+                                        alt={`Produto ${index + 1}`}
+                                        onClick={handleImageClick}
+                                    />
+                                    {/* Valor no canto inferior esquerdo */}
+                                    <div style={{ position: 'absolute', bottom: '0', left: '0', padding: '8px', background: 'rgba(255, 255, 255, 0.7)' }}>
+                                        {Number(produto.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                                    </div>
+                                        <ButtonGroup className="div-product-button-group mx-auto" style={{ position: 'absolute', bottom: '0', right: '0', display: 'flex', width: '60%' }}>
+                                            <Button onClick={() => addProdutoCarrinho(produto.id)} className="div-product-button">
+                                                Adicionar ao carrinho
+                                            </Button>
+
+                                            <Button onClick={() => addProdutoCarrinho(produto.id)} className="div-product-button">
+                                                Comprar
+                                            </Button>
+                                        </ButtonGroup>
+                                </div>
+                            ))}
+                            <SlArrowRight
+                                className="arrow right"
+                                style={{ position: 'absolute', top: '50%', right: '10px', transform: 'translateY(-50%)', fontSize: '48px', color: 'white', cursor: 'pointer' }}
+                                onClick={() => setCurrentImageIndex((prevIndex: number) => (prevIndex + 1) % produtos.length)}
+                            />
+                            <SlArrowLeft
+                                className="arrow left"
+                                style={{ position: 'absolute', top: '50%', left: '10px', transform: 'translateY(-50%)', fontSize: '48px', color: 'white', cursor: 'pointer' }}
+                                onClick={() => setCurrentImageIndex((prevIndex: number) => (prevIndex - 1 + produtos.length) % produtos.length)}
+                            />
+
                         </div>
                     </div>
                 </Container>
-
                 <div className="d-flex justify-content-around p-3 flex-wrap">
                     {produtos.map((produto: any) =>
                         <div key={"product_card_" + produto.id} className="div-product">
 
                             <Card key={"card_" + produto.id} className="div-product-card">
-                                <Card.Img variant="top" src={produto.imagem}/>
+                                <Card.Img
+                                    variant="top"
+                                    src={produto.imagem}
+                                    style={{ height: '250px', objectFit: 'cover' }} // Defina a altura desejada
+                                />
 
                                 <Card.Body className="p-2">
-                                    <Card.Text className="text-body-secondary h6 mt-1 float-end">{"R$ " + String(produto.valor.toFixed(2)).replace('.', ',')}</Card.Text>
+                                    <Card.Text className="text-body-secondary h6 mt-1 float-end"> {Number(produto.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</Card.Text>
                                     <Card.Title className="d-flex align-items-center h1">{produto.nome}</Card.Title>
-                                    <Separator/>
-                                    <Card.Text style={{textAlign: "justify"}}>{produto.descricao}</Card.Text>
-                                    <Dropdown.Divider/>
+                                    <Separator />
+                                    <Card.Text style={{ textAlign: "justify" }}>{produto.descricao}</Card.Text>
+                                    <Dropdown.Divider />
                                 </Card.Body>
                             </Card>
 
-                            <ButtonGroup className="div-product-button-group mx-auto" style={{width: 'calc(100% - 2px)'}}>
+                            <ButtonGroup className="div-product-button-group mx-auto" style={{ width: 'calc(100% - 2px)' }}>
                                 <Button onClick={() => addProdutoCarrinho(produto.id)} className="div-product-button">
                                     Adicionar ao carrinho
                                 </Button>
