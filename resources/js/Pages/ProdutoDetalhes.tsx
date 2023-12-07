192b2d841bfcfac3ef40393d9dea16c5ce4ddb42
CRISTIAN VALENTINI <cristian.valentini@universo.univates.br>
2023-12-07 15:23:25 -0300

    CalculaCepInicio

--- resources/js/Pages/ProdutoDetalhes.tsx
+++ resources/js/Pages/ProdutoDetalhes.tsx
@@ -24,6 +24,10 @@ type ProdutoDetalhesProps = PageProps<{
     descricao: string;
     imagem: string;
     valor: number;
+    altura: number;
+    largura: number;
+    comprimento: number;
+    peso: number;
     // Outros campos do produto que você deseja exibir
   };
 }>;
@@ -47,7 +51,7 @@ const ProdutoDetalhes: React.FC<ProdutoDetalhesProps> = ({ auth, produto }) => {
                 className="img-fluid"
                 src={produto.imagem}
                 alt={produto.nome}
-                style={{ width: '100%', height: 'auto', borderRadius: '5px' }}
+                style={{ width: '100%', height: '800px', borderRadius: '5px' }}
               />
             </div>
           </div>
@@ -56,38 +60,49 @@ const ProdutoDetalhes: React.FC<ProdutoDetalhesProps> = ({ auth, produto }) => {
             <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px' }}>
               <p>{produto.descricao}</p>
             </div>
-            <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px', marginTop: '15px', width: '50%', marginLeft: 'auto', textAlign: 'right' }}>
-              <div className="d-flex justify-content align-items-center mb-2">
-                <label htmlFor="quantidade" style={{ marginLeft: '15px', marginRight: '2px' }}>Quantidade:</label>
-                <input type="number" id="quantidade" className="form-control" min="1" value={quantidade} onChange={handleQuantidadeChange} style={{ width: '70px' }} />
-                <p className='ml-10 mb-0'>
-                  Total: {Number(produto.valor * quantidade).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
-                </p>
+            <div className='d-flex'>
+              <div className='w-50'>
+                <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px', marginTop: '15px', marginRight: '15px', textAlign: 'center' }}>
+                  <h5 className='mb-4'>Dimensões do Produto</h5>
+                  <div style={{ textAlign: 'left' }}>
+                    <p>Altura: {produto.altura} cm</p>
+                    <p>Largura: {produto.largura} cm</p>
+                    <p>Comprimento: {produto.comprimento} cm</p>
+                    <p>Peso: {produto.comprimento} Kg</p>
+                  </div>
+                </div>
               </div>
-              <ButtonGroup className="div-product-button-group mx-auto">
-                <Button onClick={() => addProdutoCarrinho(produto.id, quantidade)} className="btn btn-outline-success text-white" style={{ width: '60%', opacity: 0.8 }}>
-                  Adicionar ao carrinho
-                </Button>
-                <Button onClick={() => addProdutoCarrinho(produto.id, quantidade)} className="btn btn-outline-success text-white" style={{ width: '40%', opacity: 0.8 }}>
-                  Comprar
-                </Button>
-              </ButtonGroup>
-            </div>
-            <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px', marginTop: '15px', width: '50%', marginLeft: 'auto'}}>
-              <div>
-                <h5>Informe seu CEP para calcular o Frete</h5>
-                <form action="" className="ml-5 form-inline formShipping">
-                  <input style={{border:'0px',borderRadius:'5px'}} 
-                    placeholder="99999-999"
-                    type="number"
-                  />
-                    <ButtonGroup className="div-product-button">
-                    <Button className="btn btn-outline-success ml-5 text-white">Calcular</Button>
+              <div className='w-50'>
+                <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px', marginTop: '15px', marginLeft: 'auto', textAlign: 'right' }}>
+                  <div className="d-flex justify-content align-items-center mb-2">
+                    <label htmlFor="quantidade" style={{ marginLeft: '15px', marginRight: '2px' }}>Quantidade:</label>
+                    <input type="number" id="quantidade" className="form-control" min="1" value={quantidade} onChange={handleQuantidadeChange} style={{ width: '70px' }} />
+                    <p className='ml-10 mb-0'>
+                      Total: {Number(produto.valor * quantidade).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                    </p>
+                  </div>
+                  <ButtonGroup className="div-product-button-group mx-auto">
+                    <Button onClick={() => addProdutoCarrinho(produto.id, quantidade)} className="btn btn-outline-success text-white" style={{ width: '60%', opacity: 0.8 }}>
+                      Adicionar ao carrinho
+                    </Button>
+                    <Button onClick={() => addProdutoCarrinho(produto.id, quantidade)} className="btn btn-outline-success text-white" style={{ width: '40%', opacity: 0.8 }}>
+                      Comprar
+                    </Button>
                   </ButtonGroup>
-                </form>
+                </div>
+                <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '5px', marginTop: '15px', marginLeft: 'auto' }}>
+                  <h5>Informe seu CEP para calcular o Frete</h5>
+                  <form action="" className="ml-5 form-inline formShipping">
+                    <input style={{ border: '0px', borderRadius: '5px' }}
+                      placeholder="99999-999"
+                      type="number" />
+                    <ButtonGroup className="div-product-button">
+                      <Button className="btn btn-outline-success ml-5 text-white">Calcular</Button>
+                    </ButtonGroup>
+                  </form>
+                </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
